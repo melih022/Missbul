be5561f51408925757a3df925a3c21f4f70ceee8
melih022 <110200865+melih022@users.noreply.github.com>
2022-12-25 01:38:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5715936867:AAEvplFt2NR9dpsgibN0mNZeJ_dg_jPNESk")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5807124690:AAFxwdMtdp3GWWWMafj510QHl0QmHd2mKr0")
     API_ID = int(os.environ.get("API_ID", "4723828"))
     API_HASH = os.environ.get("API_HASH", "a41aa20922b2b9ed39ecbcffcd452154")
     BOT_OWNER = os.environ.get("BOT_OWNER", "MissMuzik")
