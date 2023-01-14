40067652f2ec7da7f33747dd2381b1302de7e0d9
melih022 <110200865+melih022@users.noreply.github.com>
2023-01-14 16:26:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config:
     API_HASH = os.environ.get("API_HASH", "a41aa20922b2b9ed39ecbcffcd452154")
     BOT_OWNER = os.environ.get("BOT_OWNER", "MissSahip")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoMuzikAsistan")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "MissMuzikKayit")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "VideoMuzikKayit")
     GROUP = os.environ.get("GROUP", "SohbetMavi") 
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001711522921"))
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001661213368"))
