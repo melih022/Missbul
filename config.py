ab94c15e1852a3dfff33fbec9d52b029c580a6a4
melih022 <110200865+melih022@users.noreply.github.com>
2023-04-06 00:10:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "5807124690:AAFxwdMtdp3GWWWMafj510QHl0QmHd2mKr0")
     API_ID = int(os.environ.get("API_ID", "4723828"))
     API_HASH = os.environ.get("API_HASH", "a41aa20922b2b9ed39ecbcffcd452154")
-    BOT_OWNER = os.environ.get("BOT_OWNER", "MissSahip")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "İntikamkurucusu")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoMuzikAsistan")
     PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "VideoMuzikKayit")
     GROUP = os.environ.get("GROUP", "SohbetMavi") 
