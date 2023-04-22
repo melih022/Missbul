6b4b31f8a27c5292d9a2e797b1c2d9b77f312078
melih022 <110200865+melih022@users.noreply.github.com>
2023-04-23 00:20:44 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ import os
 class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "6151655209:AAEuzgAAaq_MR47U8wYrCh9bKRBgUVrn1-g")
     API_ID = int(os.environ.get("API_ID", "20305957"))
-    API_HASH = os.environ.get("API_HASH", "02f612321d53994c6a607ac2ba03206a)
+    API_HASH = os.environ.get("API_HASH", "02f612321d53994c6a607ac2ba03206a")
     BOT_OWNER = os.environ.get("BOT_OWNER", "DeepBotsventor")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "DeepVcBot")
     PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "DeepBotsMusic")
