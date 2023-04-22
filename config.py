c43ff758b5356814dffe6eaddd0a82ce0f871f12
melih022 <110200865+melih022@users.noreply.github.com>
2023-04-23 00:15:38 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5807124690:AAFxwdMtdp3GWWWMafj510QHl0QmHd2mKr0")
-    API_ID = int(os.environ.get("API_ID", "4723828"))
-    API_HASH = os.environ.get("API_HASH", "a41aa20922b2b9ed39ecbcffcd452154")
-    BOT_OWNER = os.environ.get("BOT_OWNER", "İntikamkurucusu")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "VideoMuzikAsistan")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "VideoMuzikKayit")
-    GROUP = os.environ.get("GROUP", "SohbetMavi") 
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001711522921"))
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6151655209:AAEuzgAAaq_MR47U8wYrCh9bKRBgUVrn1-g")
+    API_ID = int(os.environ.get("API_ID", "20305957"))
+    API_HASH = os.environ.get("API_HASH", "02f612321d53994c6a607ac2ba03206a)
+    BOT_OWNER = os.environ.get("BOT_OWNER", "DeepBotsventor")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "DeepVcBot")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "DeepBotsMusic")
+    GROUP = os.environ.get("HAKKIMDA", "DeepBotsOfficial") 
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001948617476"))
