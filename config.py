4f176979c6d70701da017025eae680995c89e720
melih022 <110200865+melih022@users.noreply.github.com>
2023-06-05 21:20:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6151655209:AAEuzgAAaq_MR47U8wYrCh9bKRBgUVrn1-g")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6157942511:AAEn-TCf4vOam72acFdg4_fM4U6NxejeToY")
     API_ID = int(os.environ.get("API_ID", "20305957"))
     API_HASH = os.environ.get("API_HASH", "02f612321d53994c6a607ac2ba03206a")
-    BOT_OWNER = os.environ.get("BOT_OWNER", "DeepBotsventor")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "DeepVcBot")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "DeepBotsMusic")
+    BOT_OWNER = os.environ.get("BOT_OWNER", "Ireax")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "GoogleMuzikBot")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "GoogleMuzikKayıt")
     GROUP = os.environ.get("HAKKIMDA", "DeepBotsOfficial") 
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001948617476"))
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001711522921"))
