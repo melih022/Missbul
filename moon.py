6237288df0f02736f47ee0eb7443faceb95a66fc
melih022 <110200865+melih022@users.noreply.github.com>
2023-08-24 01:27:05 +0300

    Update moon.py

--- moon.py
+++ moon.py
@@ -12,12 +12,20 @@ from pyrogram.types import (
     InlineKeyboardMarkup,
     Message
 )
+
+from telethon.sync import TelegramClient
+from telethon.sessions import StringSession
+from telethon.tl.functions.messages import GetDialogs
+from telethon.tl.types import InputPeerEmpty
+from telethon.sync import events
+
 import requests
 import telegram
 from datetime import datetime
 from pyrogram.types import CallbackQuery
 
 
+deep = TelegramClient('bot', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
 
 #config#
 
@@ -132,18 +140,20 @@ async def son_durum(event):
     await event.respond(f"**Grup sayısı 🤖**\n\nToplam Grup: `{len(grup_sayi)}`")
                         
 
-# "/statik" komutu işleyici
-@bot.on_message(filters.command('statik') & ~filters.edited)
-async def grup_bilgileri(_, message):
-    user_id = message.from_user.id
+## "/statik" komutu işleyici
+@deep.on(events.NewMessage(pattern='/statik(?:\s+(.*))?', incoming=True))
+async def grup_bilgileri(event):
+    global anlik_calisan,grup_sayi,ozel_list
+    user_id = event.sender_id
     
     if user_id in ozel_list:
-        param = message.text.split()[1] if len(message.text.split()) > 1 else None
+        args = event.pattern_match.group(1)
         
-        if not param:
-            await message.reply(f"Toplam grup sayısı: `{len(grup_sayi)}`")
-        elif param.isdigit():
-            member_count = int(param)
+        if not args:
+            total_group_count = await bot.get_dialogs_count()
+            await event.reply(f"Toplam grup sayısı: `{len(grup_sayi)}`")
+        elif args.isdigit():
+            member_count = int(args)
             less_than = 0
             more_than = 0
             async for dialog in bot.iter_dialogs():
@@ -151,11 +161,14 @@ async def grup_bilgileri(_, message):
                     less_than += 1
                 elif dialog.is_group:
                     more_than += 1
-            await message.reply(f"{less_than} grupta {member_count}'ten az üye var. {more_than} grupta ise daha fazla.")
+            await event.reply(f"{less_than} grupta {member_count}'ten az üye var. {more_than} grupta ise daha fazla.")
         else:
-            await message.reply("Geçerli bir sayı belirtiniz.")
+            await event.reply("Geçerli bir sayı belirtiniz.")
     else:
-        await message.reply("Bu komutu kullanma izniniz yok.")
+        await event.reply("Bu komutu kullanma izniniz yok.")
+
+# Botu çalıştır
+
     
 #@client.on(events.NewMessage(pattern='^/statik (\d+)'))
 #async def grup_bilgileri_uyeler(event):
@@ -308,4 +321,7 @@ def bul(_, message):
         
         
 
+
+# Botu çalıştırın
+deep.run_until_disconnected()
 bot.run()
