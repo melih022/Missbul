99ec651d54ea4a9c41e0542f175a1d9687796827
melih022 <110200865+melih022@users.noreply.github.com>
2023-10-03 22:59:57 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     API_ID = int(os.environ.get("API_ID", "20305957"))
     API_HASH = os.environ.get("API_HASH", "02f612321d53994c6a607ac2ba03206a")
     BOT_OWNER = os.environ.get("BOT_OWNER", "lreax")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "GoogleMuzikBot")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "GoogleMuziksBot")
     PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "GoogleMuzikKayıt")
     GROUP = os.environ.get("HAKKIMDA", "GoogleBilgi") 
     PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001711522921"))
