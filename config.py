c2796f00b7ddd378b0b17332509ee47f1921a0ea
melih022 <110200865+melih022@users.noreply.github.com>
2024-03-10 16:17:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6188130506:AAF_YV9Aa2ErP6pPNHlypaSsmKthluBZ8BQ")
-    API_ID = int(os.environ.get("API_ID", "20305957"))
-    API_HASH = os.environ.get("API_HASH", "02f612321d53994c6a607ac2ba03206a")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6188130506:AAGieqMNYDVBtsMdCI9GhEsWwGSplthmjVs")
+    API_ID = int(os.environ.get("API_ID", "28167888"))
+    API_HASH = os.environ.get("API_HASH", "d27cbb6932219fcf1bfd6137485c5bac")
     BOT_OWNER = os.environ.get("BOT_OWNER", "lreax")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "GoogleMuziksBot")
     PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "GoogleMuzikKayıt")
