c9cddc3e2a091c846e09f1b64f004b102ceec899
melih022 <110200865+melih022@users.noreply.github.com>
2026-03-24 19:21:48 +0300

    Update bot configuration with new tokens and IDs

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 
 class Config:
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6188130506:AAGieqMNYDVBtsMdCI9GhEsWwGSplthmjVs")
-    API_ID = int(os.environ.get("API_ID", "28167888"))
-    API_HASH = os.environ.get("API_HASH", "d27cbb6932219fcf1bfd6137485c5bac")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "8431999361:AAHyc531MlXYioVSSq3-KIontgyFG7W7qTw")
+    API_ID = int(os.environ.get("API_ID", "27992427"))
+    API_HASH = os.environ.get("API_HASH", "07342c40853340995a492d0fc2de96cf")
     BOT_OWNER = os.environ.get("BOT_OWNER", "lreax")
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "GoogleMuziksBot")
-    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "GoogleMuzikKayıt")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Deepmusicdenemeebot")
+    PLAYLIST_NAME = os.environ.get("PLAYLIST_NAME", "sohbetGoogle")
     GROUP = os.environ.get("HAKKIMDA", "GoogleBilgi") 
-    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001711522921"))
+    PLAYLIST_ID = int(os.environ.get("PLAYLIST_ID", "-1001977465611"))
